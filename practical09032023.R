149196c17753b5a3749d2a4bbb801a826154041c
unknown <l00171335@atu.ie>
2023-03-09 12:28:13 +0000

    initial commit

---
+++ practical09032023.R
@@ -0,0 +1,21 @@
+x <- 5 + 7
+x
+y <- x - 3
+y
+
+
+z <- c(1.1, 3, 3.10)
+
+zCombined <- c(z,555,z)
+
+zFun <- (z*2)+100
+
+zFun
+
+my_sqrt <- sqrt(z-1)
+my_sqrt
+
+
+a <- c(1, 2, 3, 4) + c(1 , 10)
+
+a
